25e49be68f3b04b0ae318a063124be01f88b4d2e
ysh4296 <dbtmdgns4296@naver.com>
2021-12-08 17:30:49 +0900

    merging

--- app/src/main/java/com/example/soup/Current_State.java
+++ app/src/main/java/com/example/soup/Current_State.java
@@ -4,4 +4,9 @@ public class Current_State {
     int player_health_point;
     int monster_health_point;
     int turns;
+    Current_State(int player_health_point,int monster_health_point, int turns){
+        this.player_health_point = player_health_point;
+        this.monster_health_point = monster_health_point;
+        this.turns = turns;
+    }
 }
